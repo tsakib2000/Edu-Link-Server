9084b64d3109e5604712ac42659f4584400d85c4
tsakib2000 <isakib49@gmail.com>
2025-01-17 22:22:30 +0600

    made user role update api

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ async function run() {
         return res.status(401).send({ message: "forbidden access" });
       }
       const token = req.headers.authorization.split(" ")[1];
-  
+
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
           return res.status(401).send({ message: "forbidden access" });
         }
-      
+
         req.decoded = decoded;
         next();
       });
@@ -55,16 +55,16 @@ async function run() {
     //verify user IsAdmin
     app.get("/user/admin/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
-      if (email !== req?.decoded?.email){
-        return res.status(403).send({ message: "unauthorized access" })
+      if (email !== req?.decoded?.email) {
+        return res.status(403).send({ message: "unauthorized access" });
       }
-        const query={email}
-        const user= await userCollection.findOne(query);
-        let admin = false
-        if(user){
-          admin = user.role === 'admin';
-        }
-        res.send({admin})
+      const query = { email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user.role === "admin";
+      }
+      res.send({ admin });
     });
     //Users related api
     app.post("/users", async (req, res) => {
@@ -76,9 +76,29 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+    //update user to admin
+    app.patch('/user/:id',async(req,res)=>{
+const role=req.body;
+const id=req.params.id;
+const query={_id:new ObjectId(id)};
+const updateRole={
+  $set:role
+}
+const result = await userCollection.updateOne(query,updateRole)
+res.send(result)
+    })
     //Get All users
     app.get("/users", verifyToken, async (req, res) => {
-      const result = await userCollection.find().toArray();
+      const search =req.query.search;
+      let query={};
+      query={
+        email:{
+          $regex:String(search),
+          $options:'i',
+        }
+      }
+   
+      const result = await userCollection.find(query).toArray();
       res.send(result);
     });
     // get single user
@@ -102,16 +122,16 @@ async function run() {
       res.send(result);
     });
     //get approved sessions
-    app.get('/session/:status/:email',async(req,res)=>{
-      const status= req.params.status
-      const email =req.params.email;
-      const query={
+    app.get("/session/:status/:email", verifyToken,async (req, res) => {
+      const status = req.params.status;
+      const email = req.params.email;
+      const query = {
         status,
-        tutorEmail:email
-      }
-      const result = await sessionCollection.find(query).toArray()
-      res.send(result)
-    })
+        tutorEmail: email,
+      };
+      const result = await sessionCollection.find(query).toArray();
+      res.send(result);
+    });
     //get single session by id
     app.get("/session/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
@@ -154,44 +174,48 @@ async function run() {
     });
 
     //upload materials to db
-    app.post('/materials',async(req,res)=>{
-      const materials=req.body;
+    app.post("/materials", verifyToken,async (req, res) => {
+      const materials = req.body;
       const result = await materialCollection.insertOne(materials);
+      res.send(result);
+    });
+    //get all materials
+    app.get('/materials',async(req,res)=>{
+      const result = await materialCollection.find().toArray()
       res.send(result);
     })
     //view tutor materials
-    app.get('/materials/:email',verifyToken,async(req,res)=>{
-      const email=req.params.email;
-      const query= {tutorEmail:email}
-      const result = await materialCollection.find(query).toArray()
-      res.send(result)
-
-    })
-    //get single material by ID 
-    app.get('/material/:id',async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
+    app.get("/materials/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { tutorEmail: email };
+      const result = await materialCollection.find(query).toArray();
+      res.send(result);
+    });
+    //get single material by ID
+    app.get("/material/:id", verifyToken,async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const result = await materialCollection.findOne(query);
-      res.send(result)
-    })
-    // update material 
-    app.patch('/material/:id',async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)};
-      const materials=req.body;
-      const updateMaterial={
-        $set:materials
-      }
-      const result = await materialCollection.updateOne(query,updateMaterial)
-      res.send(result)
-    })
+      res.send(result);
+    });
+    // update material
+    app.patch("/material/:id", verifyToken,async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const materials = req.body;
+      const updateMaterial = {
+        $set: materials,
+      };
+      const result = await materialCollection.updateOne(query, updateMaterial);
+      res.send(result);
+    });
     //delete material
-    app.delete('/material/:id',async(req,res)=>{
-const id= req.params.id;
-const query={_id:new ObjectId(id)}
-const result = await materialCollection.deleteOne(query);
-res.send(result);
-    })
+    app.delete("/material/:id", verifyToken,async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await materialCollection.deleteOne(query);
+      res.send(result);
+    });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
